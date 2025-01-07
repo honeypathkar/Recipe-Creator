89c50c278d48aaea6b139be247e6b83fec06a43a
honeypathkar <honeypatkar70@gmail.com>
2025-01-07 13:36:55 +0530

    Setting up tailwind css

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,11 @@
 import React from "react";
 
-export default function App() {
-  return <div>Hello World</div>;
+function App() {
+  return (
+    <div className="flex justify-center items-center h-screen bg-gray-100">
+      <h1 className="text-4xl font-bold text-red-500">Hello, Tailwind CSS!</h1>
+    </div>
+  );
 }
+
+export default App;
